42d1ea91a0738b923cbee9de0dccc47cace03e3d
dariuszkolasa <dariuszmillenniumhs@gmail.com>
2025-03-03 00:09:01 -0500

    HW#3

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovment : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class PlayerMovment : MonoBehaviour
     private float _xVelocity = 0f;
     private float _yVelocity = 0f;
     public float speed = 3;
+    public string armory = "Armory";
 
     // Start is called before the first frame update
     void Start()
@@ -35,4 +37,19 @@ public class PlayerMovment : MonoBehaviour
         
         _rigRigidbody2D.velocity = new Vector2(_xVelocity, _yVelocity) * speed; 
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Teleporter":
+                {
+                    SceneManager.LoadScene(armory);
+                    break;
+                }
+        }
+            
+
+    }
 }
+
